08dbb3612eae0df6b4241e6a5122e55da3fdbb10
mile <mile.miljanovic@devoteam.com>
2019-12-04 13:09:20 +0100

    Test

---
+++ hello/src/main/java/com/sdlc/hello/HelloService.java
@@ -0,0 +1,16 @@
+package com.sdlc.hello;
+
+import java.sql.Timestamp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+	
+	@Bean
+	public void hello() {
+		System.out.println("Hello there friend! Current date and time is: " + new Timestamp(System.currentTimeMillis()));
+	}
+
+}
